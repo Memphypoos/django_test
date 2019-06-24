8eac5521db60059ea22e6b8dbf1d105387a7eb6d
Memphypoos <idrinkshampoo@gmail.com>
2019-06-24 14:07:37 +0800

    Create views.py

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+
+# Create your views here.
+class HomePageView(TemplateView):
+  template_name = 'home.html'
+
+class ReadMeView(TemplateView):
+  template_name = 'readme.html'
